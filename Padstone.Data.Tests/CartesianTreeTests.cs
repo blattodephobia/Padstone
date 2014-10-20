650ce5016398dcb3f6e207a5c809887c271b106e
zombunny <hselldf398@gmail.com>
2014-10-20 16:19:07 +0300

    CartesianTree: Added support for both Min and Max range queries in, first implementation steps for IList.

--- Padstone.Data.Tests/CartesianTreeTests.cs
+++ Padstone.Data.Tests/CartesianTreeTests.cs
@@ -7,34 +7,77 @@ namespace Padstone.Data.Tests
     public class CartesianTreeTests
     {
         [TestMethod]
-        public void Test_NonRepeatingSequence()
+        public void Test_Min_NonRepeatingSequence()
         {
             CartesianTree<int> testTree = new CartesianTree<int>();
             int[] sample = new int[] { 9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5 };
             testTree.AddRange(sample);
-            Assert.AreEqual(1, testTree.Min(0, sample.Length));
-            Assert.AreEqual(8, testTree.Min(4, 7));
-            Assert.AreEqual(10, testTree.Min(6, 8));
+            Assert.AreEqual(1, testTree.RangeExtremum(0, sample.Length));
+            Assert.AreEqual(8, testTree.RangeExtremum(4, 7));
+            Assert.AreEqual(10, testTree.RangeExtremum(6, 8));
+            Assert.AreEqual(7, testTree.RangeExtremum(2, 3));
         }
 
         [TestMethod]
-        public void Test_RepeatingSequence()
+        public void Test_Min_RepeatingSequence()
         {
             CartesianTree<int> testTree = new CartesianTree<int>();
             int[] sample = new int[] { 9, 9, 7, 7, 8, 12, 5, 20, 8, 18, 5 };
             testTree.AddRange(sample);
-            Assert.AreEqual(9, testTree.Min(0, 1));
-            Assert.AreEqual(8, testTree.Min(4, 5));
-            Assert.AreEqual(5, testTree.Min(4, sample.Length));
+            Assert.AreEqual(9, testTree.RangeExtremum(0, 1));
+            Assert.AreEqual(8, testTree.RangeExtremum(4, 5));
+            Assert.AreEqual(5, testTree.RangeExtremum(4, sample.Length));
         }
 
         [TestMethod]
-        public void Test_Single()
+        public void Test_Min_Single()
         {
             CartesianTree<int> testTree = new CartesianTree<int>();
             int[] sample = new int[] { 9 };
             testTree.AddRange(sample);
-            Assert.AreEqual(9, testTree.Min(0, 1));
+            Assert.AreEqual(9, testTree.RangeExtremum(0, 1));
+        }
+        [TestMethod]
+        public void Test_Max_NonRepeatingSequence()
+        {
+            CartesianTree<int> testTree = new CartesianTree<int>(true);
+            int[] sample = new int[] { 9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5 };
+            testTree.AddRange(sample);
+            Assert.AreEqual(20, testTree.RangeExtremum(0, sample.Length));
+            Assert.AreEqual(12, testTree.RangeExtremum(4, 7));
+            Assert.AreEqual(18, testTree.RangeExtremum(9, 11));
+        }
+
+        [TestMethod]
+        public void Test_Max_RepeatingSequence()
+        {
+            CartesianTree<int> testTree = new CartesianTree<int>(true);
+            int[] sample = new int[] { 9, 9, 7, 7, 8, 12, 5, 20, 8, 18, 5 };
+            testTree.AddRange(sample);
+            Assert.AreEqual(9, testTree.RangeExtremum(0, 2));
+            Assert.AreEqual(8, testTree.RangeExtremum(4, 5));
+            Assert.AreEqual(20, testTree.RangeExtremum(4, sample.Length));
+            Assert.AreEqual(12, testTree.RangeExtremum(0, 7));
+        }
+
+        [TestMethod]
+        public void Test_Max_Single()
+        {
+            CartesianTree<int> testTree = new CartesianTree<int>(true);
+            int[] sample = new int[] { 9 };
+            testTree.AddRange(sample);
+            Assert.AreEqual(9, testTree.RangeExtremum(0, 1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void Test_UnsupportedType()
+        {            
+            CartesianTree<object> testTree = new CartesianTree<object>();
+            testTree.AddRange(new object[] { 1, 2, 3, 4, 5 });
+            Assert.Fail();
+            // by this point an exception has to have been thrown, because there is no way to compare System.Object without using a custom
+            // IEqualityComparer or Comparison; if no exception has been thrown after both the constructor or the Add/AddRange method, fail the test
         }
     }
 }
--- Padstone.Data/CartesianTree.cs
+++ Padstone.Data/CartesianTree.cs
@@ -6,23 +6,41 @@ using System.Threading.Tasks;
 
 namespace Padstone.Data
 {
-    public class CartesianTree<T>
+    public class CartesianTree<T> : IList<T>
     {
         private CartesianTreeNode<T> root;
 
-        private int IComparableResult(T x, T y)
+        private static int IComparableResult(T x, T y)
         {
             IComparable<T> _x = x as IComparable<T>;
             return _x.CompareTo(y);
         }
 
+        private Comparison<T> GetInvertedComparison(Comparison<T> comparison)
+        {
+            Comparison<T> result = delegate (T x, T y)
+            {
+                return -comparison.Invoke(x, y);
+            };
+
+            return result;
+        }
+
         private Comparison<T> Compare;
-        private int itemsCount = 0;
+
+        private List<T> listInternal = new List<T>();
+
+        private void Initialize(IEnumerable<T> collection, Comparison<T> comparison, bool selectMaxValues)
+        {
+            this.Compare = selectMaxValues ? this.GetInvertedComparison(comparison) : comparison;
+            this.IsMaxQueryTree = selectMaxValues;
+            this.AddRange(collection);
+        }
 
         public void AddRange(IEnumerable<T> collection)
         {
             CartesianTreeNode<T> current = null;
-            int currentIndex = this.itemsCount - 1;
+            int currentIndex = this.Count - 1;
             foreach (T item in collection)
             {
                 currentIndex++;
@@ -58,13 +76,14 @@ namespace Padstone.Data
                 {
                     this.root = current;
                 }
+                listInternal.Add(item);
             }
         }
 
-        public T Min(int rangeInclusiveStart, int rangeExclusiveEnd)
+        public T RangeExtremum(int rangeInclusiveStart, int rangeExclusiveEnd)
         {
             CartesianTreeNode<T> current = this.root;
-            while (current.Index < rangeInclusiveStart || current.Index > rangeExclusiveEnd)
+            while (current.Index < rangeInclusiveStart || current.Index >= rangeExclusiveEnd)
             {
                 if (current.Index < rangeInclusiveStart)
                 {
@@ -79,37 +98,124 @@ namespace Padstone.Data
             return current.Value;
         }
 
+        public T this[int index]
+        {
+            get
+            {
+                return this.listInternal[index];
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.listInternal.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool IsMaxQueryTree { get; private set; }
+
         public CartesianTree() :
-            this(Enumerable.Empty<T>())
+            this(Enumerable.Empty<T>(), false)
         {
         }
 
-        private bool AllTrue(Type type, object criteria)
+        public CartesianTree(bool selectMaxValues) :
+            this(Enumerable.Empty<T>(), selectMaxValues)
         {
-            return true;
         }
 
-        public CartesianTree(IEnumerable<T> collection)
+        public CartesianTree(IEnumerable<T> collection, bool selectMaxValues = false)
         {
             Type icomparableType = typeof(IComparable<T>);
             if (typeof(T).FindInterfaces((type, criteria) => type == icomparableType, null).Any())
             {
-                this.Compare = this.IComparableResult;
+                Comparison<T> comparison = IComparableResult;
+                this.Initialize(collection, comparison, selectMaxValues);
             }
-
-            this.AddRange(collection);
+            else
+            {
+                throw new NotSupportedException(string.Format("Type {0} doesn't implement IComparable<{0}>", typeof(T).FullName));
+            }
         }
 
-        public CartesianTree(IEnumerable<T> collection, IComparer<T> comparer)
+        public CartesianTree(IEnumerable<T> collection, IComparer<T> comparer, bool selectMaxValues = false)
         {
-            this.Compare = comparer.Compare;
-            this.AddRange(collection);
+            Comparison<T> comparison = comparer.Compare;
+            this.Initialize(collection, comparison, selectMaxValues);
         }
 
-        public CartesianTree(IEnumerable<T> collection, Comparison<T> comparison)
+        public CartesianTree(IEnumerable<T> collection, Comparison<T> comparison, bool selectMaxValues = false)
         {
-            this.Compare = comparison;
-            this.AddRange(collection);
+            this.Initialize(collection, comparison, selectMaxValues);
         }
+
+        #region IList<T> methods
+
+        public int IndexOf(T item)
+        {
+            return this.listInternal.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            this.listInternal.Clear();
+            this.root = null;
+        }
+
+        public bool Contains(T item)
+        {
+            return this.listInternal.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            this.listInternal.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.listInternal.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
     }
 }
