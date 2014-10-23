6743147ac8fa6953586e92ef3143b66cbf190386
zombunny <hselldf398@gmail.com>
2014-10-23 14:57:17 +0300

    Added custom ICommand implementations.

---
+++ Padstone.Xaml/CommandBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Padstone.Xaml
+{
+    public abstract class CommandBase : DependencyObject, ICommand
+    {
+        public static readonly DependencyProperty CustomCommandProperty =
+            DependencyProperty.RegisterAttached(
+            PropertyName.FromDependencyProperty(() => CustomCommandProperty),
+            typeof(ICommand),
+            typeof(CommandBase),
+            new PropertyMetadata(null));
+
+        public static ICommand GetCustomCommand(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(CustomCommandProperty);
+        }
+
+        public static void SetCustomCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(CustomCommandProperty, value as ICommand);
+        }
+
+        public event EventHandler CanExecuteChanged;
+        
+        protected void RaiseCanExecuteChanged()
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Forces subscribers to this command to call CanExecute again.
+        /// </summary>
+        public void InvalidateCanExecute()
+        {
+            this.RaiseCanExecuteChanged();
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+}
---
+++ Padstone.Xaml/DelegateCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Padstone.Xaml
+{
+    public class DelegateCommand : CommandBase
+    {
+        private static readonly Func<bool> CanAlwaysExecute = () => { return true; };
+
+        private Action<object> action;
+        private Func<bool> canExecuteCondition;
+        private Func<object, bool> canExecuteConditionParameterized;
+        
+        public override bool CanExecute(object parameter)
+        {
+            if (this.canExecuteCondition != null)
+            {
+                return this.canExecuteCondition.Invoke();
+            }
+            else if (this.canExecuteConditionParameterized != null)
+            {
+                return this.canExecuteConditionParameterized.Invoke(parameter);
+            }
+            else
+            {
+                return CanAlwaysExecute.Invoke();
+            }
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (this.action != null)
+            {
+                this.action.Invoke(parameter);
+            }
+        }
+
+        public DelegateCommand(Action<object> action) :
+            this(action, CanAlwaysExecute)
+        {
+        }
+
+        public DelegateCommand(Action<object> action, Func<bool> canExecuteCondition)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            this.action = action;
+            this.canExecuteCondition = canExecuteCondition;
+        }
+
+        public DelegateCommand(Action<object> action, Func<object, bool> canExecuteCondition)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            this.action = action;
+            this.canExecuteConditionParameterized = canExecuteCondition;
+        }
+    }
+}
