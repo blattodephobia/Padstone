760a0ce2c4d40457b458a4bac976a40296002bcd
blattodephobia <hselldf398@gmail.com>
2015-11-18 10:38:01 +0200

    Added untracked files.

---
+++ Padstone.Data/IRangeExtremumQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Padstone
+{
+    public interface IRangeExtremumQuery<T>
+	{
+		T FindMinimum(int lowerInclusiveBound, int upperInclusiveBound);
+
+		T FindMaximum(int lowerInclusiveBound, int upperInclusiveBound);
+	}
+}
